c54df23076e990af8e4c083932987f1de1554a6e
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-06 20:36:41 +0700

    send with name

--- index.js
+++ index.js
@@ -17,8 +17,13 @@ io.on('connection', socket => {
         if (arrUsername.indexOf(username) !== -1) {
             return socket.emit('XAC_NHAN_DANG_KY', false);
         }
+        socket.username = username;// eslint-disable-line
         socket.emit('XAC_NHAN_DANG_KY', arrUsername);
         arrUsername.push(username);
         io.emit('NGUOI_DUNG_MOI', username);
+    });
+
+    socket.on('NGUOI_DUNG_GUI_TIN', message => {
+        io.emit('TIN_NHAN_MOI', `${socket.username}: ${message}`);
     });
 });
--- src/app.js
+++ src/app.js
@@ -23,6 +23,15 @@ $('document').ready(() => {
         }
         alert('Username da ton tai!');
     });
+
+    $('#btnSend').click(() => {
+        const message = $('#txtMessage').val();
+        socket.emit('NGUOI_DUNG_GUI_TIN', message);
+    });
+
+    socket.on('TIN_NHAN_MOI', message => {
+        $('#ulMessage').append(`<li>${message}</li>`)
+    })
 });
 
 //https://socket.io/docs/emit-cheatsheet/
